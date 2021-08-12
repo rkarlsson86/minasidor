d3a9c911ca55c07de5b6c009a3546876faa47f62
Wincey <vincent@sylleb.io>
2021-08-12 15:22:05 +0200

    refactor: removed unused variable

--- libs/api/configuration/src/lib/validation.ts
+++ libs/api/configuration/src/lib/validation.ts
@@ -5,7 +5,4 @@ export const validationSchema = Joi.object({
   APP_HOST: Joi.string().alphanum().default('localhost'),
   APP_PORT: Joi.number().default(8080),
   APP_DOMAIN: Joi.string().default('http://localhost:8080'),
-  JWT_SECRET: Joi.string().default('Thisis@Mysuper@Secret!'),
-  JWT_EXPIRED: Joi.string().default('7d'),
-  DATABASE_URL: Joi.string().default('mysql://reminderDB:reminderDB@localhost:3307/reminderDB?schema=public'),
 })
