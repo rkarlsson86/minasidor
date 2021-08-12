550011b08b28e375c57eab9e4057e5183ec0aea6
Wincey <vincent@sylleb.io>
2021-08-12 15:44:31 +0200

    feat: configure our app to use the routes

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiConfigModule } from '@xact-checkout/api/config';
+import { RouterModule } from 'nest-router'
+import { routes } from '../routes'
 
 @Module({
   imports: [
     ApiConfigModule,
+    RouterModule.forRoutes(routes),
   ],
   controllers: [AppController],
   providers: [AppService],
