b02d79fa0b1c3c5c1ab533ee1f8e00f687414771
Wincey <vincent@sylleb.io>
2021-08-13 09:40:24 +0200

    feat: add nft tiles to the home page

---
+++ libs/web/home/ui/tile/src/lib/home-tile.component.spec.ts
@@ -0,0 +1,20 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+import { HomeTileComponent } from './home-tile.component'
+
+
+describe('RoseIndicatorComponent', () => {
+  let spectator: Spectator<HomeTileComponent>;
+
+  const createComponent = createComponentFactory({
+    component: HomeTileComponent,
+    imports: [RouterTestingModule],
+    shallow: true
+  });
+
+  beforeEach(() => (spectator = createComponent()));
+
+  it('should create', () => {
+    expect(spectator.component).toBeTruthy();
+  });
+});
---
+++ libs/web/home/ui/tile/src/lib/home-tile.component.ts
@@ -0,0 +1,14 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core'
+
+@Component({
+  selector: 'xact-checkout-home-tile',
+  templateUrl: './home-tile.component.html',
+  styles: [
+    ``,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HomeTileComponent {
+  constructor() {
+  }
+}
